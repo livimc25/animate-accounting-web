133dcb226ca05bb9dc67cd128eb06d49c28edeaa
Alivia McGuire <gracefulliv@gmail.com>
2022-02-21 21:40:46 -0500

    feat: Add all necessary routes

--- app/router.js
+++ app/router.js
@@ -7,4 +7,18 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function() {
+  this.route('what-is-accounting');
+  this.route('why-learn-accounting');
+  this.route('accounting-labs', function() {
+    this.route('accounting-equation');
+    this.route('revenue-recognition-and-matching');
+    this.route('income-statement');
+    this.route('balance-sheet');
+    this.route('stockholders-equity');
+  });
+  this.route('getting-started', function() {
+    this.route('what-is-accounting');
+    this.route('why-learn-accounting');
+  });
+  this.route('stockholders-equity');
 });
